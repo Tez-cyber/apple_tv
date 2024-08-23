6b18277f871307854264f291ade8ea54da45199a
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-23 07:40:56 +0100

    Added custom colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      bgDark: "#000",
+      bgContrast: "#111",
+      textDark: "#1d1d1f",
+      light: "#fff"
+    },
     extend: {},
   },
   plugins: [],
