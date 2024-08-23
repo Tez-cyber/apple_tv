a8f74a41c6ba58a5ebbc696e4ee2fe7721c66244
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-23 16:30:14 +0100

    Installed react-icons and imported icon in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
+import { FaApple } from "react-icons/fa";
 
 const Header = () => {
     return (
         <>
             <header className="bg-bgContrast p-6 text-light">
-                
+                <a href="/" className="text-light">
+                    <FaApple />
+                </a>
             </header>
+            <div className="">
+                Apple Tv +
+            </div>
         </>
     )
 }
