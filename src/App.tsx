3fe31e455b9a4cee0bc966b72a504f5dab7e0a54
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-01 18:43:55 +0200

    Added Container Wrapper to site layout in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { Container } from "./components/Container";
 import Header from "./components/Header";
 
 export default function App() {
   return (
-      <>
-        <Header />
-        {/* ==== Project layout */}
-        <main className="">
-            <div className="h-[200vh]">Hero component</div>
-            <div>usps</div>
-            <div className="">3 col layout</div>
-            <div className="">Carousel with posters</div>
-        </main>
-      </>
+    <>
+      <Header />
+      {/* ==== Project layout */}
+      <main className="">
+        <div className="h-[200vh]">
+          <Container>Hero component</Container>
+        </div>
+        <div>
+          <Container>usps</Container>
+        </div>
+        <div className="">
+          <Container>3 col layout</Container>
+        </div>
+        <div className="">
+          <Container>Carousel with posters</Container>
+        </div>
+      </main>
+    </>
   );
 }
