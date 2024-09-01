1f181f25ed5e2ad5d35025150b8e4dc04465ea18
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-01 19:21:47 +0200

    Added custom font-size to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ export default {
       textDark: "#1d1d1f",
       light: "#fff"
     },
+    fontSize: {
+      xs: ".75rem", //== 12px
+      sm: ".875rem", //== 14px
+      base: "1.0625rem", //== 17px
+      lg: "1.1875rem", //== 12px
+      xl: ".75rem", //== 19px
+      "2xl": "1.5rem", //== 21px
+      "3xl": "1.75rem", //== 24px
+      "4xl": "2.5rem", //== 40px
+      "5xl": "4.5rem", //== 72px
+    },
     extend: {},
   },
   plugins: [],
