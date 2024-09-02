84d61dd44ad9afc4961e1082d2fb9c4eb4a88730
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-02 07:48:32 +0200

    Added root classes to index.css

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -4,11 +4,13 @@ import { Container } from "../Container"
 
 export const Hero = () => {
     return (
-        <div className="h-[200vh] bg-bgDark text-light">
-            <img src="/posters/img-4.jpg" alt="" />
-            <Container>
+        <div className="h-[200vh] relative bg-bgDark text-light">
+            <div className="absolute top-0 left-0 w-full">
+                <img src="/posters/img-4.jpg" alt="" />
+            </div>
+            <Container className="relative min-h-[100svh] flex flex-col justify-end">
                 <h1 className="text-5xl font-bold">
-                    All Apple Originals. <br /> 
+                    All Apple Originals. <br />
                     Only on Apple TV+.
                 </h1>
                 <Button size="lg">
