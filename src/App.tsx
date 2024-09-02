9b3949786b6b065f3112330c5493ab52c255aceb
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-02 09:44:37 +0200

    Created a parent div with bg to wrap Hero and Usps

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ export default function App() {
       <Header />
       {/* ==== Project layout */}
       <main className="">
-        <Hero />
-        <Usps />
+        <div className="bg-bgDark">
+          <Hero />
+          <Usps />
+        </div>
         <div className="">
           <Container>3 col layout</Container>
         </div>
