fc33d42e4c4c29ad680ddab28fc1a5a68a443ba4
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-02 09:42:46 +0200

    Added text content for Usps section

--- src/components/section/Usps.tsx
+++ src/components/section/Usps.tsx
@@ -2,6 +2,18 @@ import { Container } from "../Container"
 
 export const Usps = () => {
   return (
-    <Container>usps</Container>
+    <Container className="text-3xl font-semibold">
+        <p>New Apple Originals every month - always ad-free</p>
+        <p>
+            Stream on the Apple TV app on Apple devices, smart Tvs, consoles, or
+            sticks
+        </p>
+        <p>
+            Watch in 4K HDR video with immersive Spatial Audio
+        </p>
+        <p>
+            Share a single subcription with up to five people.
+        </p>
+    </Container>
   )
 }
