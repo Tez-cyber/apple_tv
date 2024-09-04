b26039f15462cb801730749bde103b7e48f138cd
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-04 14:26:43 +0100

    Created child component SmallVideoCarousel in VideoCarousel.tsx to render small carousel

--- src/components/section/Video-carousel.tsx
+++ src/components/section/Video-carousel.tsx
@@ -1,14 +1,34 @@
-import { Container } from "../Container"
+import { Movie, movies, randomMoviesSet1, randomMoviesSet2 } from "../movies"
 
 export const VideoCarousel = () => {
     return (
         <div className="bg-bgDark">
             {/* ==== Large first section ==== */}
-            <div className="">3 large videos</div>
+            {/* <div className="">
+                <div className=""><img src={movies[0].poster} alt={movies[0].name} /></div>
+                <div className=""><img src={movies[0].poster} alt={movies[0].name} /></div>
+                <div className=""><img src={movies[0].poster} alt={movies[0].name} /></div>
+            </div> */}
 
             {/* ==== carousel ==== */}
-            <div className=""></div>
-            <div className=""></div>
+            <SmallVideoCarousel movies={randomMoviesSet1} />
+            <SmallVideoCarousel movies={randomMoviesSet2} />
+        </div>
+    )
+}
+
+const SmallVideoCarousel = ({movies}: {movies: Movie[]}) => {
+    return (
+        <div className="flex gap-3">
+            {
+                movies.map((movie, i) => (
+                    <img
+                        key={`${movie.name}-${i}`}
+                        src={movie.poster}
+                        alt={movie.name}
+                    />
+                ))
+            }
         </div>
     )
 }
