466c7ed194a460bc02a226f8940e50365c653cdb
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-04 07:24:41 +0100

    Imported Fade-in and wrapped all p tags in Usps

--- src/components/section/Usps.tsx
+++ src/components/section/Usps.tsx
@@ -1,19 +1,28 @@
 import { Container } from "../Container"
+import { FadeIn } from "../Fade-in"
 
 export const Usps = () => {
   return (
     <Container className="text-4xl max-w-[692px] z-10 relative py-36 font-semibold text-light space-y-12">
+      <FadeIn>
         <p>New Apple Originals every month - always ad-free</p>
+      </FadeIn>
+      <FadeIn>
         <p>
-            Stream on the Apple TV app on Apple devices, smart Tvs, consoles, or
-            sticks
+          Stream on the Apple TV app on Apple devices, smart Tvs, consoles, or
+          sticks
         </p>
+      </FadeIn>
+      <FadeIn>
         <p>
-            Watch in 4K HDR video with immersive Spatial Audio
+          Watch in 4K HDR video with immersive Spatial Audio
         </p>
+      </FadeIn>
+      <FadeIn>
         <p>
-            Share a single subcription with up to five people.
+          Share a single subcription with up to five people.
         </p>
+      </FadeIn>
     </Container>
   )
 }
