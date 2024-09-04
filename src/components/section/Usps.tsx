f4d13d882f07674c141b0b163bea528bf8169bab
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-04 07:17:27 +0100

    Added vertical padding to Usps.tsx

--- src/components/section/Usps.tsx
+++ src/components/section/Usps.tsx
@@ -2,7 +2,7 @@ import { Container } from "../Container"
 
 export const Usps = () => {
   return (
-    <Container className="text-4xl font-semibold text-light space-y-12">
+    <Container className="text-4xl max-w-[692px] z-10 relative py-36 font-semibold text-light space-y-12">
         <p>New Apple Originals every month - always ad-free</p>
         <p>
             Stream on the Apple TV app on Apple devices, smart Tvs, consoles, or
