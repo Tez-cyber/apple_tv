7e3b4b4c92bc5b1636e98abc70acb65baf288db1
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-04 12:46:39 +0100

    Added type Movie to movie.ts

--- src/components/movies.ts
+++ src/components/movies.ts
@@ -1,6 +1,9 @@
+export type Movie = {
+    poster: string
+    name: string
+}
 
-
-const movies = [
+export const movies = [
     { poster: "/posters/plane.jpg", name: "Airplane"},
     { poster: "/posters/img-2.jpg", name: "Family man"},
     { poster: "/posters/lab.jpg", name: "Laboratory"},
@@ -10,13 +13,13 @@ const movies = [
     { poster: "/posters/stars.jpg", name: "Stars"},
 ]
 
-const randomMoviesSet1 = movies
+export const randomMoviesSet1 = movies
     .sort(() => Math.random() - 0.5)
     .concat(movies.sort(() => Math.random() - 0.5))
     .concat(movies.sort(() => Math.random() - 0.5))
 
 
-const randomMoviesSet2 = movies
+export const randomMoviesSet2 = movies
     .sort(() => Math.random() - 0.5)
     .concat(movies.sort(() => Math.random() - 0.5))
     .concat(movies.sort(() => Math.random() - 0.5))
