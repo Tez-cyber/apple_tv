e93384812c0c0492e82d93ac4918275132c1863a
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-04 08:41:07 +0100

    Created movies.ts and added movies array

---
+++ src/components/movies.ts
@@ -0,0 +1,11 @@
+
+
+const movies = [
+    { poster: "", name: "Airplane"},
+    { poster: "", name: "Family man"},
+    { poster: "", name: "Laboratory"},
+    { poster: "", name: "Napoleon"},
+    { poster: "", name: "Person in Darkness"},
+    { poster: "", name: "Scary Building"},
+    { poster: "", name: "Stars"},
+]
