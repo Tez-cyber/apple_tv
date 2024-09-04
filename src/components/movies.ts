cd0c643a02906437fd76b9f5bbb0fb7ac9c758bd
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-04 12:39:24 +0100

    Added images to public/posters and added two arrays to movies.ts

--- src/components/movies.ts
+++ src/components/movies.ts
@@ -1,11 +1,23 @@
 
 
 const movies = [
-    { poster: "", name: "Airplane"},
-    { poster: "", name: "Family man"},
-    { poster: "", name: "Laboratory"},
-    { poster: "", name: "Napoleon"},
-    { poster: "", name: "Person in Darkness"},
-    { poster: "", name: "Scary Building"},
-    { poster: "", name: "Stars"},
+    { poster: "/posters/plane.jpg", name: "Airplane"},
+    { poster: "/posters/img-2.jpg", name: "Family man"},
+    { poster: "/posters/lab.jpg", name: "Laboratory"},
+    { poster: "/posters/napoleon.jpg", name: "Napoleon"},
+    { poster: "/posters/darkness.jpg", name: "Person in Darkness"},
+    { poster: "/posters/scary.jpg", name: "Scary Building"},
+    { poster: "/posters/stars.jpg", name: "Stars"},
 ]
+
+const randomMoviesSet1 = movies
+    .sort(() => Math.random() - 0.5)
+    .concat(movies.sort(() => Math.random() - 0.5))
+    .concat(movies.sort(() => Math.random() - 0.5))
+
+
+const randomMoviesSet2 = movies
+    .sort(() => Math.random() - 0.5)
+    .concat(movies.sort(() => Math.random() - 0.5))
+    .concat(movies.sort(() => Math.random() - 0.5))
+    .sort(() => Math.random() - 0.5)
