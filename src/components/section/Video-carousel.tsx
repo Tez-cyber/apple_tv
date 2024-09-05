2f0ec5e05c5db195a2550eb6a48bf4b65465cb58
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-05 13:41:26 +0100

    Added scroll animation to first section

--- src/components/section/Video-carousel.tsx
+++ src/components/section/Video-carousel.tsx
@@ -1,18 +1,27 @@
+import { useScroll, useTransform, motion } from "framer-motion"
 import { Movie, movies, randomMoviesSet1, randomMoviesSet2 } from "../movies"
+import { useRef } from "react"
 
 export const VideoCarousel = () => {
+    const carouselWrapperRef = useRef<HTMLDivElement>(null)
+    const { scrollYProgress } = useScroll({
+        target: carouselWrapperRef,
+        offset: ["start start", "end start"],
+    })
+    const scale = useTransform(scrollYProgress, [0.3, .5, .66], [3, 2.5, 1])
+
     return (
         <div className="bg-bgDark pb-8">
             {/* ==== Large first section ==== */}
-            <div className="overflow-clip h-[300vh] mt-[-100vh]">
-                <div className="h-screen sticky top-0">
-                    <div className="flex gap-5 mb-5 left-1/2 -translate-x-1/2">
+            <div ref={carouselWrapperRef} className="overflow-clip h-[300vh] mt-[-100vh]">
+                <div className="h-screen sticky top-0 flex items-center">
+                    <div className="flex gap-5 mb-5 relative left-1/2 -translate-x-1/2">
                         <div className="shrink-0 w-[60vw] aspect-video rounded-2xl overflow-clip">
                             <img className="w-full h-full object-cover" src={movies[0].poster} alt={movies[0].name} />
                         </div>
-                        <div className="shrink-0 w-[60vw] aspect-video rounded-2xl overflow-clip">
+                        <motion.div style={{ scale }} className="shrink-0 w-[60vw] aspect-video rounded-2xl overflow-clip">
                             <img className="w-full h-full object-cover" src={movies[1].poster} alt={movies[0].name} />
-                        </div>
+                        </motion.div>
                         <div className="shrink-0 w-[60vw] aspect-video rounded-2xl overflow-clip">
                             <img className="w-full h-full object-cover" src={movies[2].poster} alt={movies[0].name} />
                         </div>
